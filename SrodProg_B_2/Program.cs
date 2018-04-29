a40ae45717440a07e09f936b59ce1b4380fb8551
Slawomir <SJaniga@outlook.com>
2018-04-29 09:06:01 +0200

    Add files via upload

---
+++ SrodProg_B_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Slaw is sharp with C#");
+            int number = 100;
+            Console.WriteLine(number);
+            Console.WriteLine("Enter number: ", number);
+            number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(number);
+
+            int[] tabx = new int [3];
+            tabx[0] = 12;
+            tabx[1] = 11;
+            tabx[2] = 1500;
+            for (int i = 0; i < 3; i++)
+                Console.WriteLine(tabx[i]);
+            
+            Console.ReadKey();
+        }
+    }
+}
